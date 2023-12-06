b5bbc27208a07876750e3a3321f3898b124e54e1
NainaRam <nainaram7980@gmail.com>
2023-12-06 14:10:51 +0530

    new files

---
+++ dictionary.py
@@ -0,0 +1,12 @@
+#dictionaries
+#creating a dictionaries 
+# dictionries are always in pairs of key and value
+details = {"name" :"raju", "age":21, "color": "fair"}
+print(type(details))
+print(details)
+print(len(details))
+print(details.items())
+details.keys()
+details.values()
+details["gender"]="male"
+print(details)
---
+++ tuples.py
@@ -0,0 +1,12 @@
+# this is the tuples practise
+fruits=("guava","apple", " mango","papaya")
+fruitss =["guava","apple", " mango","papaya"]
+print(type(fruits))
+print(fruits[3:])
+print(len(fruits))
+vegetables = ("potato","tomato","capcicum","carrot")
+print(fruits + vegetables)
+if(fruits == fruitss):{
+    print("yes")
+}
+print("ok")

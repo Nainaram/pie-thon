e2603530bfd84b6a62d9abde6b25b5f244d5e1c6
NainaRam <nainaram7980@gmail.com>
2023-12-07 10:07:29 +0530

    added new file

---
+++ typeconvesion.py
@@ -0,0 +1,18 @@
+#Convert to string
+a=10000
+
+print(str(a))
+#Convert to int
+b="2013"
+print(int(b))
+
+#Convert to float
+print(float(b))
+#CONVERT TO LIST
+S="AEIOU"
+print(S)
+print(list(S))
+#convert to set
+x = ["sai","raj","kanna","probro"]
+print(set(S))
+print(set(x))

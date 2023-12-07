f9b7c80f35ca05965abd0b9ca202b0e803d37479
NainaRam <nainaram7980@gmail.com>
2023-12-07 11:14:24 +0530

    control flows part-1

---
+++ controlflows.py
@@ -0,0 +1,31 @@
+# this file includes all the control flows statements
+# a = int(input(" Enter the value of the a:"))
+# if( a > 10000):
+#     print(" yes a is greated value" + str(a))
+# else:
+#     print("no this is not sufficient value" + str(a))
+    
+#nested if esle
+# a = int(input(" Enter the value for a:"))
+# b = int(input("Enter the second value b:"))
+# if(a == b):
+#     if(a<=100000):
+#         print(" it is in between 10000 and 100000")
+#     elif(a<50000):
+#         print(" it is in between 10000  and 50000")
+#     else:
+#         print(a)
+# else:
+#     print("please enter the sufficient value")
+# student = {"name":"ram",'age':21}
+# for i in student:
+#     print(student)
+fruits = ["apple","carrot","veggie"]
+i = 1
+for item in fruits:
+    print ("Fruit-%d:%s" %(i,item) )
+    i=i+1
+    
+    
+
+    

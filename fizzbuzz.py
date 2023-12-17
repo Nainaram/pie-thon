25d8ffe7756a6d33bfa68b78d57b7980876b0ee9
NainaRam <nainaram7980@gmail.com>
2023-12-17 13:21:54 +0530

    added new fizzbuzz

---
+++ fizzbuzz.py
@@ -0,0 +1,11 @@
+# this is the   simple program that print 1 to 100 numbers but at the
+# mutiples of 3 and 5 it prints fizz and buzz
+for num in range(1, 101):
+    if num % 3 == 0 and num % 5 == 0:
+        print("FizzBuzz")
+    elif num % 3 == 0:
+        print("Fizz")
+    elif num % 5 == 0:
+        print("Buzz")
+    else:
+        print(num)
